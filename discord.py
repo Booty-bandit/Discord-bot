6d7f8ed7622b51c9793a6a3265a258cbc57539a2
6yw858tc9q-stack <6yw858tc9q@privaterelay.appleid.com>
2026-08-17 09:01:31 +0200

    første upload

---
+++ discord.py
@@ -0,0 +1,19 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+TOK_FILE = os.path.join(os.path.dirname(__file__), "token.txt")
+
+def get_token():
+  tokfile = open(TOK_FILE, 'r')
+  token = tokfile.read()
+  tokfile.close()
+  return token
+
+@client.event
+async def on_ready():
+    print("Connected!")
